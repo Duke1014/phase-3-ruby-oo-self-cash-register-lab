391e6add1cd3070042bfaf65d499e466c584e22f
duke1014 <duke.norsworthy@gmail.com>
2021-11-01 13:28:29 -0400

    commit tree ade6e0b3631dbe5b3a6db0fc6d85c712b39ecfe7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+require 'pry'
+class CashRegister
 
+    attr_accessor :discount, :total, :quantity, :title, :price
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @item_list = []
+    end
+    def add_item(title, price, quantity = 1)
+        @price = price
+        @quantity = quantity
+        @total = self.total + price * quantity
+        quantity.times { @item_list << title }
+    end
+    def apply_discount
+        if discount > 0
+            @total = self.total - self.total*(self.discount/100.to_f)
+            "After the discount, the total comes to $#{self.total.round}."
+        else
+            "There is no discount to apply."
+        end
+    end
+    def items
+        @item_list
+    end
+    def void_last_transaction
+        # binding.pry
+        @quantity.times { @total = @total - @price }
+        @item_list.pop
+        # binding.pry
+        if @item_list.length == 0
+            @total = 0.0
+        end
+    end
+end
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 describe 'CashRegister' do
   let(:cash_register) { CashRegister.new }
   let(:cash_register_with_discount) { CashRegister.new(20) }
@@ -80,6 +81,7 @@ describe 'CashRegister' do
         new_register = CashRegister.new
         new_register.add_item("eggs", 1.99, 2)
         new_register.add_item("tomato", 1.76, 3)
+        # binding.pry
         expect(new_register.items).to eq(["eggs", "eggs", "tomato", "tomato", "tomato"])
       end
     end
